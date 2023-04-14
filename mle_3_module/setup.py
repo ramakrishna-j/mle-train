9502b7d547d6ea292845b82d0ebfafb6437fe2e1
ramakrishna.j <ramakrishna.j@tigeranalytics.com>
2023-04-14 14:16:15 +0530

    docker session

--- mle_3_module/setup.py
+++ mle_3_module/setup.py
@@ -4,10 +4,15 @@ import os
 from setuptools import find_packages, setup
 
 setup(
-    name="housing_pkg",
+    name="ramakrishna_j_package",
     version="0.0.1",
     author="Rama Krishna",
-    description="Ingest data, train and score models",
-    long_description="Make various models to predict house price, score each model and log all the details",
-    packages=find_packages(),
+    description="House Price prediction",
+    author_email = 'ramakrishna.j@tigeranalytics.com',
+    packages=find_packages(where='src'),
+    install_requires=[
+        'requests',
+        'importlib-metadata; python_version == "3.8"',
+    ],
+    package_dir = {"":"src"},
 )
